3b3dca9426ba6bd6d9a5893cad241b5cc224a0f7
kcwh99 <54660484+kcwh99@users.noreply.github.com>
2020-08-16 17:24:05 +0800

    Update password

--- src/app/myprofile/myprofile.page.ts
+++ src/app/myprofile/myprofile.page.ts
@@ -3,12 +3,12 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { UserService } from '../user.service';
 import { FirebaseService } from '../services/firebase.service';
 import { Router } from '@angular/router';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
 import { AuthService } from '../auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 interface Profile {
-  id:string
+  id:string;
   username: string;
   email: string;
   password: string;
@@ -20,40 +20,40 @@ interface Profile {
 })
 export class MyprofilePage implements OnInit {
   profile: Profile;
-
+  refUserDoc: AngularFirestoreDocument
   ProfileList: [];
   user: any;
-
-
+  sub;
+  newPassword:string;
+  UID:string;
   constructor( 
+    private userService:UserService,
     private router: Router, 
     private firebaseService: FirebaseService, 
     private firestore: AngularFirestore,
     private afAuth: AngularFireAuth,
     private authService: AuthService, 
     private fb: FormBuilder) {
-    this.user = this.afAuth.auth.currentUser
-    console.log(this.user)
+    this.user = this.afAuth.auth.currentUser;
+    this.UID = this.user.uid;
+    console.log(this.UID);
+    console.log(this.user.displayName);
+    console.log(this.user.email);
     this.profile = {} as Profile
+    //this.refUserDoc = this.firestore.doc(`users/${this.user.uid}`)
    }
 
   ngOnInit() {
     console.log(this.user)
-
-    // this.authService.getProfile().subscribe(data => {
-
-    //   this.ProfileList = data.map(e => {
-    //     return {
-    //       id: e.payload.doc.id,
-    //       isEdit: false,
-    //       username : e.payload.doc.data()['username'],
-    //       email : e.payload.doc.data()['email'],
-    //       password : e.payload.doc.data()['password'],
-    //     };
-    //   })
-    //   console.log(this.ProfileList);
-
-    // });
+    
+  }
+  updatePassword(){
+    this.userService.updatePassword(this.newPassword);
+    this.profile.email = this.user.email;
+    this.profile.id = this.UID;
+    this.profile.username = this.user.displayName;
+    this.profile.password = this.newPassword;
+    this.userService.updateDatabaseProfile(this.profile,this.UID);
 
   }
 }
